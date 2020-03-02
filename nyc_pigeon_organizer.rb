c8a9e6ac8217ba08a47be5d498274de5246a095b
Ruth Obe <ruthobey@gmail.com>
2020-03-02 06:07:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,38 +1,28 @@
 require "pry"
 
-def nyc_pigeon_organizer(data)
-  pigeon_list = Hash.new
-  
-  #create an inner hash that takes color, gender, lives as keys and an array as value
+def pigeon_name_hash (list)
   inner_hash = Hash.new 
-  hash_names = data.keys
+  
+  hash_names = list.keys
+  
   hash_names.each do |key|
     inner_hash[key] = []
   end 
-  
-  # get all names from gender in data into an aoa
-  name_array = []
-  data[:gender].each do |gender_key, gender_key_value|
-    name_array << gender_key_value
+  inner_hash
+
+end
+
+def nyc_pigeon_organizer(data)
+  pigeon_list = Hash.new
+
+  data[:color].each do |color, v|
+    v.map do |name|
+      if !pigeon_list.include? name
+        pigeon_list[name] = pigeon_name_hash(data)
+        binding.pry
+      end
+    end
   end
-  
-  #flatten name_array aoa
-  name_array_flatten = name_array.flatten
-  
-  # set up pigeon_list with keys and values
-  name_array_flatten.each do |name_key|
-    pigeon_list[name_key] = inner_hash
-  end 
-  
-  male = data[:gender][:male]
-  female = data[:gender][:female]
-  
-  # pigeon_list.map do |k,v|
-  #   if !male.include? k 
-  #     pigeon_list[k][:gender].push("male")
-  #     break
-  #   end
-  # end
   
   pigeon_list
 end
