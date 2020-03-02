792734458f38e7ae7c505d64da9548d54e2a1638
Ruth Obe <ruthobey@gmail.com>
2020-03-02 06:22:02 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -26,7 +26,7 @@ def nyc_pigeon_organizer(data)
   
   data[:gender].each do |g, gv|
     # data[:lives].each do |l, lv|
-      pigeon_list[name][:gender] << g.to_s
+    pigeon_list[name][:gender] << g.to_s
       # pigeon_list[name][:lives] << l.to_s
       # binding.pry
     # end
